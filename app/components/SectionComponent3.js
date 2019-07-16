e606f627cb641706b1930f8bf08daf9b880a53e7
QOTPC007 <zyllar68>
2019-07-17 07:02:40 +0800

    section 3 is finish

--- app/components/SectionComponent3.js
+++ app/components/SectionComponent3.js
@@ -44,10 +44,10 @@ export const SectionComponentFieldThree = () => (
 
       <Column nameClass='is-5 column-in-vs-left'>
         <CustomCardSection3 titleLabel='we:{fund}' nameClass='bg-blue card-wefund'>
-          <CustomCardContent3 nClass='mt-2' imageSrc='./app/css/assets/plus.png' label='Higher annualized returns than public REITs' />
-          <CustomCardContent3 imageSrc='./app/css/assets/plus.png' label='Rental cash flows come from owners with skin in the game' />
-          <CustomCardContent3 imageSrc='./app/css/assets/plus.png' label='No latency on rental cash flows-properties are all pre-leased' />
-          <CustomCardContent3 imageSrc='./app/css/assets/plus.png' label='Technology DNA seeks highest returns possible' />
+          <CustomCardContent3 nClass='mt-2' imageSrc='./app/css/assets/plus.png' label='Provides a turn-key, hassle free investment vehicle' />
+          <CustomCardContent3 imageSrc='./app/css/assets/plus.png' label='Uses machine learning and algorithms to find you the best deals' />
+          <CustomCardContent3 imageSrc='./app/css/assets/plus.png' label='Acquisitions and dispositions team headed by leading experts with years of experience' />
+          <CustomCardContent3 imageSrc='./app/css/assets/plus.png' label='A sound, disciplined strategy spreads risk among multiple assets' />
         </CustomCardSection3>
       </Column>
 
@@ -59,10 +59,10 @@ export const SectionComponentFieldThree = () => (
 
       <Column nameClass='is-5 column-in-vs-right'>
         <CustomCardSection3 titleLabel='Traditional REIT' nameClass=' card-trads'>
-          <CustomCardContent3 nClass='mt-2' imageSrc='./app/css/assets/minus.png' label='Higher annualized returns than public REITs' />
-          <CustomCardContent3 imageSrc='./app/css/assets/minus.png' label='Cash flows come from tenants' />
-          <CustomCardContent3 imageSrc='./app/css/assets/minus.png' label='Properties can sit for months-lost income and lowered rents' />
-          <CustomCardContent3 imageSrc='./app/css/assets/minus.png' label='Finance companies which try to layer on technology' />
+          <CustomCardContent3 nClass='mt-2' imageSrc='./app/css/assets/minus.png' label='Provides a turn key, hassle free investment vehicle' />
+          <CustomCardContent3 imageSrc='./app/css/assets/minus.png' label='Uses machine learning and algorithms to find you the best deals' />
+          <CustomCardContent3 imageSrc='./app/css/assets/minus.png' label='Acquisitions and dispositions team headed by leading experts with years of experience' />
+          <CustomCardContent3 imageSrc='./app/css/assets/minus.png' label='All capital tied into 1-2 properties creates concentrated exposure of risk ' />
         </CustomCardSection3>
       </Column>
 
